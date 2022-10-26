610fdaecc12f057a5dd45afb0114d91cf39aa6b0
Hardik <sachanhardik@gmail.com>
2022-10-26 21:27:23 +0530

    added implementation for auth use cases

--- app/src/main/java/com/hardiksachan/docket/auth/auth_application/SignInWithEmailAddressAndPasswordUseCase.kt
+++ app/src/main/java/com/hardiksachan/docket/auth/auth_application/SignInWithEmailAddressAndPasswordUseCase.kt
@@ -1,6 +1,7 @@
 package com.hardiksachan.docket.auth.auth_application
 
 import arrow.core.Either
+import com.hardiksachan.docket.auth.auth_domain.AuthFacade
 import com.hardiksachan.docket.auth.auth_domain.AuthFailure
 import com.hardiksachan.docket.auth.auth_domain.EmailAddress
 import com.hardiksachan.docket.auth.auth_domain.Password
@@ -10,4 +11,13 @@ interface SignInWithEmailAddressAndPasswordUseCase {
         emailAddress: EmailAddress,
         password: Password
     ): Either<AuthFailure, Unit>
+}
+
+class SignInWithEmailAddressAndPasswordUseCaseImpl(
+    private val authFacade: AuthFacade
+) : SignInWithEmailAddressAndPasswordUseCase {
+    override suspend fun execute(
+        emailAddress: EmailAddress,
+        password: Password
+    ): Either<AuthFailure, Unit> = authFacade.signInWithEmailAndPassword(emailAddress, password)
 }
--- app/src/main/java/com/hardiksachan/docket/auth/auth_domain/AuthFailure.kt
+++ app/src/main/java/com/hardiksachan/docket/auth/auth_domain/AuthFailure.kt
@@ -1,9 +1,9 @@
 package com.hardiksachan.docket.auth.auth_domain
 
 sealed class AuthFailure {
-    object CancelledByUser: AuthFailure()
-    object InvalidToken: AuthFailure()
-    object ServerError: AuthFailure()
-    object EmailAlreadyInUse: AuthFailure()
-    object InvalidEmailAndPasswordCombination: AuthFailure()
+    data class TokenNotGenerated(val failure: Token.GenerationFailure) : AuthFailure()
+    object InvalidToken : AuthFailure()
+    object ServerError : AuthFailure()
+    object EmailAlreadyInUse : AuthFailure()
+    object InvalidEmailAndPasswordCombination : AuthFailure()
 }
